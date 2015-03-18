51d1db77e9aca51216f3799c626065685a2b6838
Quan Tran M <qtran3@ggc.edu>
2015-03-18 23:43:36 +0000

    make it better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-        
-        render text: "Welcome myApp is here"
+        render text: "Param1:  "+params[:firstname]+"    Params2:"+params[:lastname]        
+        #render text: "Welcome myApp is here"
     end
     def paramtest
         render text: "Param1:  "+params[:firstname]+"    Params2:"+params[:lastname]
